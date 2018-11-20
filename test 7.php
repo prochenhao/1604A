7f09773657b122f0ab8441dc3d28e037561acef5
prochenhao <44669316+prochenhao@users.noreply.github.com>
2018-11-20 08:38:08 +0800

    Add files via upload

---
+++ test 7.php
@@ -0,0 +1,13 @@
+<?php 
+int numberOf1(int n)
+{
+    int count = 0;
+        unsigned int flag = 1;
+            while(flag){
+            	    if(n & 1)
+                        count++;
+            	                falg = flag << 1;
+            }
+                         return count;}
+
+ ?>
