7e1b18ff692acd0021942c364767716d8ff27e9d
prochenhao <44669316+prochenhao@users.noreply.github.com>
2018-11-22 08:29:30 +0800

    Add files via upload

---
+++ test9.php
@@ -0,0 +1,17 @@
+<?php 
+function main(parm_1,param_2,param_n){
+    if(parm_1=parm_2){
+        param_n = parm_1 = 1 ?2 :2+4^(parm_1-1);
+    }
+    if(parm_1=parm_2>1){
+        param_n = 2+;
+    }
+    if(parm_1>parm_2){
+        param_n = ;
+    }
+    if(parm_1<parm_2){
+        param_n = ;
+    }
+    return param_n;
+}
+ ?>
